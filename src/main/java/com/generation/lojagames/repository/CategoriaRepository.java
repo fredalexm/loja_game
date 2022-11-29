adef344a97b599135759c5ba58a6cbbcdb731a1d
Fred Alex Martins <fred.abarros@mercadolivre.com>
2022-11-29 15:28:22 -0300

    Correcao Repository

--- src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
+++ src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
@@ -3,9 +3,11 @@ package com.generation.lojagames.repository;
 import com.generation.lojagames.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
     public List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
--- src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -3,10 +3,12 @@ package com.generation.lojagames.repository;
 import com.generation.lojagames.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Repository
 public interface ProdutoRepository  extends JpaRepository<Produto, Long> {
 
     public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
